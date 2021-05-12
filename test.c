dbc85d3737d949400c4269519ac8b119ce9d699c
Mr_CHTO && LINUX <Mr_CHTO@outlook.com>
2021-05-12 17:21:54 +0800

    0512VIM

--- test.c
+++ test.c
@@ -1,12 +1,4 @@
-#include <stdio.h>
-int main()
-{
-    int a[5], s = 0, i;
-    for (i = 0; i < 5; i++)
-    {
-        scanf("%d", &a[i]);
-        s = s + a[i];
-    }
-    printf("%d", s);
-    system("pause");
+#include<stdio.h>
+int main(){
+    printf("asddwedewd");
 }

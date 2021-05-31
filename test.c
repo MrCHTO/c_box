3a5edf98b59460d6def0e522ad1126842c78dac1
Mr_CHTO <Mr_CHTO@outlook.com>
2021-05-31 16:30:26 +0800

    VS0531

--- test.c
+++ test.c
@@ -1,12 +1,55 @@
 #include <stdio.h>
 int main()
 {
-    int a[5], s = 0, i;
-    for (i = 0; i < 5; i++)
+    int a[10], i, j, temp, flag = 0, k;
+    float b, w = 0;
+    for (i = 0; i < 10; i++)
     {
         scanf("%d", &a[i]);
-        s = s + a[i];
     }
-    printf("%d", s);
+    for (i = 0; i < 10; i++)
+    {
+        w = w + a[i];
+    }
+    printf("%.3f\n", w);
+    b = w / 10;
+    printf("%.2f\n", b);
+
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10 - 1 - i; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < 10; i++)
+    {
+        k = 0;
+        for (j = 0; j < 10; j++)
+        {
+            if (a[i] == a[j])
+            {
+                k++;
+            }
+        }
+        if (k > flag)
+        {
+            flag = k;
+            temp = a[i];
+        }
+    }
+
+    printf("%d\n", temp);
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", a[i]);
+    }
     system("pause");
 }

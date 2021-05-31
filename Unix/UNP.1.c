3a0982fbf2cf70cbfb4f8a6a4fd8ddb85ea9e771
Mr_CHTO <Mr_CHTO@outlook.com>
2021-05-31 17:02:39 +0800

    VS0531

--- Unix/UNP.1.c
+++ Unix/UNP.1.c
@@ -1,18 +1,41 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-int main(int argc,char *argv[]){
-	if(argc==3){
-		if(argv[1]=="cat"&&argv[2]=="text.txt"){
-				
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	pid_t cpid;
+	if (argc == 3)
+	{
+		if (argv[1] == "cat" && argv[2] == "text.txt")
+		{
+			cpid = fork();
+			if (cpid == -1) //Creat Error
+			{
+				exit(0);
+			}
+			else if (cpid == 0) //Creat Child
+			{
+				if (execv("/usr/bin/cat", argv) < 0)
+				{
+					perror("Error");
+				}
+				printf("Child done");
+				exit(0);
+			}
+			else //Creat Father
+			{
+				wait(&cpid);
+				printf("father done");
+			}
 		}
-		else{
-			printf("error!")
-		}	
-		
+		else
+		{
+			exit(0);
+		}
 	}
-	eles{
-		printf("error!")
+	else
+	{
+		exit(0);
 	}
 }

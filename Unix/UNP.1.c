fcfb1f60946f53b70bdb9468de8a9aa3539b1f0a
Mr_CHTO && LINUX <Mr_CHTO@outlook.com>
2021-05-31 16:33:17 +0800

    VIM0531

---
+++ Unix/UNP.1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+int main(int argc,char *argv[]){
+	if(argc==3){
+		if(argv[1]=="cat"&&argv[2]=="text.txt"){
+				
+		}
+		else{
+			printf("error!")
+		}	
+		
+	}
+	eles{
+		printf("error!")
+	}
+}

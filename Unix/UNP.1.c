508f1886d3404ee97f64e5a549e965976bdda5d5
Mr_CHTO <Mr_CHTO@outlook.com>
2021-05-31 17:14:22 +0800

    VS0531

--- Unix/UNP.1.c
+++ Unix/UNP.1.c
@@ -7,23 +7,22 @@ int main(int argc, char *argv[])
 	pid_t cpid;
 	if (argc == 3)
 	{
-		if (argv[1] == "cat" && argv[2] == "text.txt")
+
+		cpid = fork();
+		if (cpid == -1) //Creat Error
 		{
-			cpid = fork();
-			if (cpid == -1) //Creat Error
+			exit(0);
+		}
+		else if (cpid == 0) //Creat Child
+		{
+			char *execv_str[] = {"echo", "executed by execv", NULL};
+			if (execv("/usr/bin/echo", execv_str) < 0)
 			{
+				perror("error on exec");
 				exit(0);
 			}
-			else if (cpid == 0) //Creat Child
-			{
-				char *execv_str[] = {"echo", "executed by execv", NULL};
-				if (execv("/usr/bin/echo", execv_str) < 0)
-				{
-					perror("error on exec");
-					exit(0);
-				}
 
-				/*
+			/*
 				if (execv("/usr/bin/cat", argv) < 0)
 				{
 					perror("Error");
@@ -31,16 +30,11 @@ int main(int argc, char *argv[])
 				printf("Child done");
 				exit(0);
 				*/
-			}
-			else //Creat Father
-			{
-				wait(&cpid);
-				printf("father done");
-			}
 		}
-		else
+		else //Creat Father
 		{
-			exit(0);
+			wait(&cpid);
+			printf("father done");
 		}
 	}
 	else

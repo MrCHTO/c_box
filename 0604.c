05388c06f6b39f7235f28406660097981b05c8ae
Mr_CHTO <Mr_CHTO@outlook.com>
2021-06-10 18:55:46 +0800

    VS0610

---
+++ 0604.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    char a[10];
+    scanf("%s", a);
+    /*
+    gets(a);
+    
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%c  ->  ", a[i]);
+    }
+    */
+    printf("\n");
+    puts(a);
+    system("pause");
+}

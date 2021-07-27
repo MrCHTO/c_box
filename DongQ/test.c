4695831ac84b2122fd6c41d774d81d0675f4236f
Mr_CHTO_ECS <Mr_CHTO@outlook.com>
2021-07-27 23:43:52 +0800

    VS0727

--- DongQ/test.c
+++ DongQ/test.c
@@ -1,11 +1,31 @@
-#include<stdio.h>
-int c(int a,int b)
-{
-return a+b;
-}
+#include <stdio.h>
 int main()
 {
-int x,y;
-scanf("%d%d",&x,&y);
-printf("%d",c(x,y));
+    int a, b, c, d, e, f;
+    scanf("%d%d%d", &a, &b, &c);
+    d = a + b;
+    e = a + c;
+    f = b + c;
+    if (d > e)
+    {
+        if (d > f)
+        {
+            printf("%d", d);
+        }
+        else
+        {
+            printf("%d", f);
+        }
+    }
+    else
+    {
+        if (e > f)
+        {
+            printf("%d", e);
+        }
+        else
+        {
+            printf("%d", f);
+        }
+    }
 }

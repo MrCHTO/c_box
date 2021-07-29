9307ab2c15aadb58b292d7d6adc64906e39e1a8e
Mr_CHTO_ECS <Mr_CHTO@outlook.com>
2021-07-29 16:12:08 +0800

    VS0729

--- DongQ/test.c
+++ DongQ/test.c
@@ -1,31 +1,10 @@
 #include <stdio.h>
+int h(int a,int b,int c,int d,int e)
+{
+int f;
+f=a*b*c*d*e;
+return f;
+}
 int main()
 {
-    int a, b, c, d, e, f;
-    scanf("%d%d%d", &a, &b, &c);
-    d = a + b;
-    e = a + c;
-    f = b + c;
-    if (d > e)
-    {
-        if (d > f)
-        {
-            printf("%d", d);
-        }
-        else
-        {
-            printf("%d", f);
-        }
-    }
-    else
-    {
-        if (e > f)
-        {
-            printf("%d", e);
-        }
-        else
-        {
-            printf("%d", f);
-        }
-    }
-}
+i
